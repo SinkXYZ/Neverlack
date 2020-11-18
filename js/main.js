73361e9169321d9a8b99b4a24aca7942b7ef77d0
Poyq <55713441+Poyq@users.noreply.github.com>
2020-11-18 21:03:56 +0000

    haha ez

---
+++ js/main.js
@@ -0,0 +1,3 @@
+$(()=>{const light_mode=$('.light-mode-toggle');const dark_mode=$('.dark-mode-toggle');const body=$('body');light_mode.click(()=>{body.attr('color-theme','light');Cookies.set('drip-theme','light',{expires:9999});if(typeof DRIP!=='undefined'&&DRIP.THEME_CALLBACK)
+DRIP.THEME_CALLBACK('light');light_mode.hide();dark_mode.show();});dark_mode.click(()=>{body.attr('color-theme','dark');Cookies.set('drip-theme','dark',{expires:9999});if(typeof DRIP!=='undefined'&&DRIP.THEME_CALLBACK)
+DRIP.THEME_CALLBACK('dark');light_mode.show();dark_mode.hide();});});
